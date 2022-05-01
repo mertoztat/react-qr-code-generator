bb0779df18d776f155b3517cbcd5d8bc0e71859a
Mert Öztat <mertoztat@gmail.com>
2022-05-01 14:52:42 +0300

    new some changes

--- src/App.js
+++ src/App.js
@@ -43,13 +43,13 @@ function App() {
       </div>
       <div className="icons">
         <a href="https://github.com/mertoztat" className="icon">
-          <i class="fa-brands fa-github"></i>
+          <i className="fa-brands fa-github"></i>
         </a>
         <a href="https://twitter.com/mertoztat" className="icon">
-          <i class="fa-brands fa-twitter"></i>
+          <i className="fa-brands fa-twitter"></i>
         </a>
         <a href="https://www.linkedin.com/in/mertoztat" className="icon">
-          <i class="fa-brands fa-linkedin"></i>
+          <i className="fa-brands fa-linkedin"></i>
         </a>
       </div>
     </div>
